768c2f2d91f270ef9133fedb7ebc0decabc47b36
miriamdav15 <48199701+miriamdav15@users.noreply.github.com>
2020-08-14 17:27:13 +0300

    Update CreateUserController.java

--- SymphoBook-Backend/test/src/main/java/com/example/demo/CreateUserController.java
+++ SymphoBook-Backend/test/src/main/java/com/example/demo/CreateUserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,11 +28,30 @@ public class CreateUserController {
 	}
 	
 	@PostMapping("/users/createaccount") // POST /users/createaccount?Email=a@b.com&Password=abc&PhoneNumber=123&DisplayName=a
+	ResponseEntity<String> createaccount(@RequestParam String Email, @RequestParam String Password, @RequestParam String PhoneNumber, @RequestParam String DisplayName)
+			{
+						String eroareamea = newUser.newAccount(Email,Password,PhoneNumber, DisplayName);
+						switch(eroareamea) {
+						case"Created": return new ResponseEntity<>(eroareamea,HttpStatus.OK);
+												
+						case "Email already exists": return new ResponseEntity<>(eroareamea,HttpStatus.FORBIDDEN);
+												
+						case "Invalid Phone Number": return new ResponseEntity<>(eroareamea,HttpStatus.FORBIDDEN);
+													
+						case"Password is too short": return new ResponseEntity<>(eroareamea,HttpStatus.FORBIDDEN);
+													
+						default : return new ResponseEntity<>("",HttpStatus.FORBIDDEN);
+						}
+						//return new ResponseEntity<>(stringu,HttpStatus.OK);
+			}
+	
+/*	@PostMapping("/users/createaccount") // POST /users/createaccount?Email=a@b.com&Password=abc&PhoneNumber=123&DisplayName=a
 	String createaccount(@RequestParam String Email, @RequestParam String Password, @RequestParam String PhoneNumber, @RequestParam String DisplayName)
 	{
 		PhoneNumber = "+"+PhoneNumber;
+		
 		return newUser.newAccount(Email,Password,PhoneNumber, DisplayName);
-	}
+	}*/
 	//daca useru nou vrea in momentu ala sa isi adauge detalii
 	@PostMapping("/users/adddetails")
 	Boolean addDetails (@RequestParam String Description, @RequestParam String Love, @RequestParam String Birthday, @RequestParam String Favorites)
